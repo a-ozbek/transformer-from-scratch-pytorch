0f78f1631601749772f9ccb818aba4bac76ab027
Can <ozbekahmetcan@gmail.com>
2019-10-03 19:54:16 -0700

    organize nb

--- basic_transformer/utils/data.py
+++ basic_transformer/utils/data.py
@@ -15,3 +15,17 @@ def fix_seq_len(s, max_len, fill_token=0):
         s = s + [fill_token] * (max_len - len(s))
         
     return s
+
+
+def generate_fake_df():
+    """
+    Generate fake df for debugging
+    """
+    n_positive, n_negative = 5000, 5000
+    positive_label, negative_label = 'positive', 'negative'
+    positive_text = ' '.join(['good'] * 10)
+    negative_text = ' '.join(['bad'] * 10)
+    df = [(positive_text, positive_label)] * n_positive + [(negative_text, negative_label)] * n_negative
+    df = pd.DataFrame(df)
+    df.columns = ('review', 'sentiment')
+    return df
